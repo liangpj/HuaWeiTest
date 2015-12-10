f46fb247ce9a10592fd035307adcac43e097d938
david  liang <378058621@qq.com>
2015-12-10 18:21:20 +0800

    大写字母的个数

--- src/easy/NumBigChara.java
+++ src/easy/NumBigChara.java
@@ -1,5 +1,22 @@
 package easy;
 
 
+import java.util.Scanner;
+
 public class NumBigChara {
+
+    public int calcCapital(String str) {
+        int num = 0;
+        for(int i = 0; i < str.length(); ++i) {
+            if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z')
+                ++num;
+        }
+        return num;
+    }
+    public static void main(String [] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        NumBigChara tst = new NumBigChara();
+        System.out.println(tst.calcCapital(str));
+    }
 }
